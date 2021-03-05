e841ee631d72a8bacedfe53c367f330f4a97438c
Angel Villahoz-Baleta <avillaho@all-development-environment.novalocal>
2021-03-05 20:29:59 +0000

    Sixteenth version

--- development_reset.sh
+++ development_reset.sh
@@ -1,2 +1,4 @@
-docker-compose exec php_server docker-php-ext-enable xdebug
+docker-compose exec php_server \
+    docker-php-ext-enable \
+    xdebug
 make $(cat .env | xargs) xdebug
--- docker_clean.sh
+++ docker_clean.sh
@@ -1,6 +1,11 @@
-docker stop $(docker ps --filter "name=blat_blat_server_1" --filter "name=blat_mariadb_server_1" --filter "name=blat_php_server_1" -q)
-docker rm blat_blat_server_1 blat_mariadb_server_1 blat_php_server_1
-docker rmi blat_blat_server
-docker rmi blat_php_server
-docker rmi $(docker images -f "dangling=true" -q)
+docker stop $(docker ps --filter "name=blat_blat_server_1" \
+    --filter "name=blat_mariadb_server_1" \
+    --filter "name=blat_php_server_1" \
+    -q)
+docker rm blat_blat_server_1 \
+    blat_mariadb_server_1 \
+    blat_php_server_1
+docker rmi blat_blat_server \
+    blat_php_server \
+    $(docker images -f "dangling=true" -q)
 docker volume prune -f
